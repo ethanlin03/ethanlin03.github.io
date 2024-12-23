330442a6e1cf1581d64c6e6b7fde086d4e6a767d
Ethan Lin <midgetlin2003@gmail.com>
2024-12-23 13:21:04 -0800

    Added other basics to nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import GoogleIcon from '@mui/icons-material/Google';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
     return (
         <nav className="bg-gray-800 text-white">
-        <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+        <div className="container mx-auto px-4 py-6 flex justify-between items-center">
+          
+          <MenuIcon/>
           <div className="text-xl font-bold">My Portfolio</div>
+          <GoogleIcon/>
+          <LinkedInIcon/>
+          <GitHubIcon/>
           <ul className="flex space-x-6">
             <li>
               <NavLink
