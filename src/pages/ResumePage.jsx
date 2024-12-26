347e3b2a94df082b4e6819e4db1795ef7b72ab29
Ethan Lin <midgetlin2003@gmail.com>
2024-12-25 23:05:23 -0800

    Finished resume page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,10 +1,19 @@
 import NavBar from "../components/NavBar";
+import resume from "../assets/ethan_resume.pdf";
 
 const ResumePage = () => {
     return (
         <div>
             <NavBar/>
-            <h1 className="flex justify-center items-center h-screen">Resume Page</h1>
+            <div className="w-full h-[calc(100vh-100px)] flex justify-center items-center p-6">
+                <div className="max-w-2xl mx-auto w-full h-full">
+                    <iframe 
+                        className="w-full h-full"
+                        src={`${resume}#view=FitH`}
+                        title="Resume"
+                    />
+                </div>
+            </div>
         </div>
     )
 };
