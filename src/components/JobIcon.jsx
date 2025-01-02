636fdf408390bad75acf26d3d8e1447834218cd2
Ethan Lin <midgetlin2003@gmail.com>
2025-01-01 19:30:26 -0800

    Added experience description and figured out spacing

--- src/components/JobIcon.jsx
+++ src/components/JobIcon.jsx
@@ -4,38 +4,40 @@ const JobIcon = ({ jobTitle, company, description, date, leftOrRight }) => {
     return (
         <div>
             {leftOrRight === "right" ? (
-                <div className="relative flex flex-row items-top left-[94px]">
-                    <span className="whitespace-nowrap font-semibold text-right mt-8">
+                <div className="relative flex flex-row items-top left-[34px]">
+                    <span className="whitespace-nowrap font-semibold text-right mt-8 w-[400px]">
                         {date}
                     </span>
+
                     <div className="bg-white border-gray-300 border-4 rounded-full flex items-center justify-center h-[90px] w-[90px] ml-8 mr-10">
                         <div className="bg-white border-gray-200 shadow-md border rounded-full flex items-center justify-center h-16 w-16">
                             <WorkOutlineIcon className="bg-white rounded-full h-10 w-10" />
                         </div>
                     </div>
+
                     <div className="flex flex-row">
                         <div className="w-0 h-0 border-t-[10px] border-b-[10px] border-r-[10px] border-t-transparent border-b-transparent border-r-gray-500 mt-8"/>
                         
                         <div 
-                            className="bg-white bg-opacity-95 w-[400px] h-[200px] shadow-xl z-10 rounded-md p-4 border"
+                            className="bg-gray-100 bg-opacity-95 w-[450px] h-[260px] shadow-xl z-10 rounded-md p-4 border"
                             style={{ boxShadow: '0 4px 4px rgba(0, 0, 0, 0.15)' }}
                         >
                             <h1 className="text-xl font-bold">{jobTitle}</h1>
-                            <h1 className="text-lg font-semibold mb-4 italic">{company}</h1>
-                            <p className="mb-4">Placeholder</p>
+                            <h1 className="text-md font-semibold mb-4 italic">{company}</h1>
+                            <p className="mb-4 text-sm">{description}</p>
                         </div>
                     </div>
                 </div>
             ) : (
-                <div className="relative flex flex-row items-top right-[94px]">
+                <div className="relative flex flex-row items-top right-[34px]">
                     <div className="flex flex-row">
                         <div 
-                            className="bg-white bg-opacity-95 w-[400px] h-[200px] shadow-xl z-10 rounded-md p-4 border"
+                            className="bg-gray-100 bg-opacity-95 w-[450px] h-[260px] shadow-xl z-10 rounded-md p-4 border"
                             style={{ boxShadow: '0 4px 4px rgba(0, 0, 0, 0.15)' }}
                         >
                             <h1 className="text-xl font-bold">{jobTitle}</h1>
-                            <h1 className="text-lg font-semibold mb-4 italic">{company}</h1>
-                            <p className="mb-4">Placeholder</p>
+                            <h1 className="text-md font-semibold mb-4 italic">{company}</h1>
+                            <p className="mb-4 text-sm">{description}</p>
                         </div>
 
                         <div className="w-0 h-0 border-t-[10px] border-b-[10px] border-l-[10px] border-t-transparent border-b-transparent border-l-gray-500 mt-8"/>
@@ -47,7 +49,7 @@ const JobIcon = ({ jobTitle, company, description, date, leftOrRight }) => {
                         </div>
                     </div>
 
-                    <span className="whitespace-nowrap font-semibold mt-8">
+                    <span className="whitespace-nowrap font-semibold mt-8 w-[400px]">
                         {date}
                     </span>
                 </div>
--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -7,10 +7,48 @@ const ExperiencePage = () => {
         <div>
             <NavBar/>
             <div className="container mx-auto px-4 py-8 flex justify-center mt-40">
-                <div className="border-2 border-gray-300 h-[80rem] z-0"/>
-                <div className="absolute top-30 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-40">
-                    <JobIcon jobTitle="Software Quality Assurance Intern" company="WorkUp" date="October 2024 - December 2024" leftOrRight="right"/>
-                    <JobIcon jobTitle="Software Quality Assurance Intern" company="WorkUp" date="October 2024 - December 2024" leftOrRight="left"/>
+                <div className="border-2 border-gray-300 h-[1120px] z-0"/>
+                <div className="absolute top-30 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-28">
+                    <JobIcon jobTitle="Software Quality Assurance Intern" company="WorkUp" date="October 2024 - Present" leftOrRight="left" description={
+                        <ul class="list-disc pl-5">
+                            <li>
+                                Developing features like "double clicking to like" in React, JavaScript, and FireBase to improve WorkUp’s app and upon the UI before launch
+                            </li>
+                            <li>
+                                Gained familiarity with QA procedures and regularly QA tested the IOS/Web version of WorkUp
+                            </li>
+                            <li>
+                                Writing in-depth reports for developers to understand reasons for crashes and fostering a culture of high standards
+                            </li>
+                        </ul>}/>
+                    <JobIcon jobTitle="Development Intern" company="Make-A-Wish" date="September 2024 - November 2024" leftOrRight="right" description={
+                        <ul class="list-disc pl-5">
+                            <li>
+                                Worked and gained familiarity with SalesForce to help keep track of sponsorships and maintain relationships
+                            </li>
+                            <li>
+                                Handled the outreach of sponsorships and events while also curating Make-A-Wish kid stories
+                            </li>
+                            <li>
+                                Using Canva with other creative applications to design and create Make-A-Wish stories along with showcases of events
+                            </li>
+                        </ul>}/>
+                    <JobIcon jobTitle="Research Intern" company="National Science Foundation" date="June 2022 - August 2022" leftOrRight="left" description={
+                        <ul class="list-disc pl-5">
+                            <li>
+                                Developed a set of procedures that basic programmers can follow to analyze assembly and binary
+                            </li>
+                            <li>
+                                Worked in x86 and ARM architectures and gained familiarity with said architectures
+                            </li>
+                            <li>
+                                Analyzed assembly and binary through obfuscation and basic encryption
+                            </li>
+                            <li>
+                                Collaborated with peers to meet weekly goals while demonstrating leadership when groupmates needed help
+                            </li>
+                        </ul>}/>
+                    <div class="w-8 h-8 bg-gray-300 rounded-full"></div>
                 </div>
             </div>
         </div>
