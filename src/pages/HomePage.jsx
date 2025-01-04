a3bf80155084076898d045ee33048a0270141322
Ethan Lin <midgetlin2003@gmail.com>
2025-01-03 22:20:41 -0800

    Readded contact page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,6 +13,7 @@ const HomePage = () => {
             <ProjectsPage id="projectsection" needNavBar={false}/>
             <ExperiencePage id="experiencesection" needNavBar={false}/>
             <ResumePage id="resumesection" needNavBar={false}/>
+            <ContactPage id="contactsection" needNavBar={false} viewHeight={0}/>
         </div>
     )
 };
