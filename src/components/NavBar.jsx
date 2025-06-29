9937f390bb541fec3494065a39b0a7fbe77a6ebd
Ethan Lin <midgetlin2003@gmail.com>
2025-06-29 13:51:33 -0700

    Temp fixes to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,20 @@ import { motion } from "framer-motion";
 
 import logo from "../assets/e-logo.png";
 
+const buttonLinkClass = ({ isActive }) =>
+  `xs:text-xs sm:text-base transition-colors duration-300 ease-in-out
+  ${isActive
+    ? 'relative text-blue-400 font-bold after:content-[""] after:absolute after:-bottom-2 after:left-1/2 after:-translate-x-1/2 after:w-4 after:h-1 after:bg-blue-400 after:rounded-full'
+    : 'text-gray-800 hover:text-gray-400 font-bold'
+  }`;
+
 const NavBar = () => {
     return (
       <div className="mb-32 max-w-screen mx-auto">
       <nav className="fixed top-0 left-0 w-full bg-white text-gray-800 z-50 shadow-md">
         <div className="flex justify-between items-center px-6 py-6">
           <NavLink
-              to="/home"
+              to="/projects"
               className="hover:text-gray-400 transition-colors duration-300 font-bold"
           >
             <div className="flex items-center space-x-3 ">
@@ -20,30 +27,25 @@ const NavBar = () => {
               </span>
             </div>
           </NavLink>
-          <div className="flex items-center space-x-10 xs:space-x-0">
+          <div className="flex items-center space-x-10 xs:space-x-2">
             {[
               { path: '/about', label: 'About' },
-              // { path: '/projects', label: 'Projects' },
+              { path: '/projects', label: 'Projects' },
               // { path: '/experience', label: 'Experience' },
               // { path: '/resume', label: 'Resume' },
               // { path: '/contact', label: 'Contact' },
             ].map(({ path, label }) => (
-                <NavLink
-                  key={path}
-                  to={path}
-                  className={({ isActive }) =>
-                    isActive
-                      ? 'relative text-blue-400 font-bold after:content-[""] after:absolute after:-bottom-2 after:left-1/2 after:-translate-x-1/2 after:w-4 after:h-1 after:bg-blue-400 after:rounded-full'
-                      : 'text-gray-800 hover:text-gray-400 font-bold'
-                  }
-                >
+              <NavLink key={path} to={path}>
+                {({ isActive }) => (
                   <motion.button
+                    className={buttonLinkClass({ isActive })}
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}
                   >
                     {label}
                   </motion.button>
-                </NavLink>
+                )}
+              </NavLink>
             ))}
           </div>
         </div>
