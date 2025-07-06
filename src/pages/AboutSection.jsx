719b9ae06594f11ba029c846d1a0ab57575fcd03
Ethan Lin <midgetlin2003@gmail.com>
2025-07-06 13:19:30 -0700

    Added mobile responsiveness to parts of about

--- src/pages/AboutSection.jsx
+++ src/pages/AboutSection.jsx
@@ -12,95 +12,93 @@ import { motion } from "framer-motion";
 const AboutSection = () => {
     return (
         <div>
-            <div className="max-w-screen mx-auto">
-                    
-                    <div className="container mx-auto p-4 flex items-center">
-                        <div>
-                            <motion.img
-                                initial={{ opacity: 0, y: -40 }}
-                                whileInView={{ opacity: 1, y: 0 }}
-                                transition={{ duration: 0.8, ease: "easeOut" }}
-                                viewport={{ once: true }}
-                                src={me} 
-                                className="w-[80%] max-w-[300px] aspect-square object-cover rounded-full z-0 relative top-4"
-                                alt="Ethan"
-                            />
-                            <motion.div 
+            <div className="max-w-screen">
+                <div className="container mx-auto p-4 flex flex-col sm:flex-row items-center gap-10 justify-center">
+                    <div className="flex flex-col items-center space-y-2 sm:space-y-6">
+                        <motion.img
+                            initial={{ opacity: 0, y: -40 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.8, ease: "easeOut" }}
+                            viewport={{ once: true }}
+                            src={me} 
+                            className="w-[80%] max-w-[300px] aspect-square object-cover rounded-full z-0"
+                            alt="Ethan"
+                        />
+                        <motion.div 
+                            initial={{ opacity: 0, x: -40 }}
+                            whileInView={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.8, ease: "easeOut" }}
+                            viewport={{ once: true }}
+                            className="flex items-center justify-center space-x-4 sm:space-x-8 relative bg-white px-4 py-2 max-w-[250px] border border-black border-opacity-30 shadow-lg rounded-md z-10"
+                        >
+                            <motion.a 
                                 initial={{ opacity: 0, x: -40 }}
                                 whileInView={{ opacity: 1, x: 0 }}
                                 transition={{ duration: 0.8, ease: "easeOut" }}
                                 viewport={{ once: true }}
-                                className="flex items-center justify-center space-x-8 relative bg-white px-4 py-2 max-w-[250px] border border-black border-opacity-30 shadow-lg rounded-md z-10 bottom-2 left-4"
+                                href="mailto:ethanclin03@gmail.com" target="_blank" rel="noopener noreferrer"
                             >
-                                <motion.a 
-                                    initial={{ opacity: 0, x: -40 }}
-                                    whileInView={{ opacity: 1, x: 0 }}
-                                    transition={{ duration: 0.8, ease: "easeOut" }}
-                                    viewport={{ once: true }}
-                                    href="mailto:ethanclin03@gmail.com" target="_blank" rel="noopener noreferrer"
-                                >
-                                    <IconButton className="hover:bg-gray-200 hover:scale-125">
-                                        <GoogleIcon className="text-red-500" style={{ fontSize: 26 }}/>
-                                    </IconButton>
-                                </motion.a>
+                                <IconButton className="hover:bg-gray-200 hover:scale-125">
+                                    <GoogleIcon className="text-red-500" style={{ fontSize: 26 }}/>
+                                </IconButton>
+                            </motion.a>
 
-                                <motion.a 
-                                    initial={{ opacity: 0, x: -40 }}
-                                    whileInView={{ opacity: 1, x: 0 }}
-                                    transition={{ duration: 0.8, ease: "easeOut" }}
-                                    viewport={{ once: true }}
-                                    href="https://www.linkedin.com/in/ethanlin03" target="_blank" rel="noopener noreferrer"
-                                >
-                                    <IconButton className="hover:bg-gray-200 hover:scale-125">
-                                        <LinkedInIcon className="text-blue-600" style={{ fontSize: 30 }}/>
-                                    </IconButton>
-                                </motion.a>
-
-                                <motion.a 
-                                    initial={{ opacity: 0, x: -40 }}
-                                    whileInView={{ opacity: 1, x: 0 }}
-                                    transition={{ duration: 0.8, ease: "easeOut" }}
-                                    viewport={{ once: true }}
-                                    href="https://www.github.com/ethanlin03" target="_blank" rel="noopener noreferrer"
-                                >
-                                    <IconButton className="hover:bg-gray-200 hover:scale-125"> 
-                                        <GitHubIcon className="text-gray-700" style={{ fontSize: 26 }}/>
-                                    </IconButton>
-                                </motion.a>
-                            </motion.div>
-                        </div>
-                        <motion.div 
-                            initial={{ opacity: 0, x: 40 }}
-                            whileInView={{ opacity: 1, x: 0 }}
-                            transition={{ duration: 0.8, ease: "easeOut" }}
-                            viewport={{ once: true }}
-                            className="relative bg-white bg-opacity-95 p-4 max-w-md shadow-lg left-4 rounded-md"
-                            style={{ boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}
-                        >
-                            <motion.h1 
-                                initial={{ opacity: 0, y: -40 }}
-                                whileInView={{ opacity: 1, y: 0 }}
+                            <motion.a 
+                                initial={{ opacity: 0, x: -40 }}
+                                whileInView={{ opacity: 1, x: 0 }}
                                 transition={{ duration: 0.8, ease: "easeOut" }}
                                 viewport={{ once: true }}
-                                className="text-2xl font-bold mb-2"
+                                href="https://www.linkedin.com/in/ethanlin03" target="_blank" rel="noopener noreferrer"
                             >
-                                About Me
-                            </motion.h1>
-                            <p className="mb-4">
-                                Hi, I'm Ethan! I'm a Computer Science student at the University of California, Irvine. 
-                                My journey for programming started in 2015, where my curiosity for games met my love 
-                                for creativity and creation.
-                            </p>
-                            <p className="mb-4">
-                                I've been able to use my skills to create a twist on a classic game, Rock Paper Scissors, 
-                                to creating a website to help track my medications. Through each project, I've been able to build upon my technical skills while pushing 
-                                my innovativeness and creativity. Let's connect and create fresh, exciting applications 
-                                for the world!
-                            </p>
-                        </motion.div>                        
-                    </div>
+                                <IconButton className="hover:bg-gray-200 hover:scale-125">
+                                    <LinkedInIcon className="text-blue-600" style={{ fontSize: 30 }}/>
+                                </IconButton>
+                            </motion.a>
 
-                <div className="container px-4 pb-8 mb-10">
+                            <motion.a 
+                                initial={{ opacity: 0, x: -40 }}
+                                whileInView={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 0.8, ease: "easeOut" }}
+                                viewport={{ once: true }}
+                                href="https://www.github.com/ethanlin03" target="_blank" rel="noopener noreferrer"
+                            >
+                                <IconButton className="hover:bg-gray-200 hover:scale-125"> 
+                                    <GitHubIcon className="text-gray-700" style={{ fontSize: 26 }}/>
+                                </IconButton>
+                            </motion.a>
+                        </motion.div>
+                    </div>
+                    <motion.div 
+                        initial={{ opacity: 0, x: 40 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 0.8, ease: "easeOut" }}
+                        viewport={{ once: true }}
+                        className="relative bg-white bg-opacity-95 p-4 max-w-screen sm:max-w-md shadow-lg rounded-md"
+                        style={{ boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}
+                    >
+                        <motion.h1 
+                            initial={{ opacity: 0, y: -40 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.8, ease: "easeOut" }}
+                            viewport={{ once: true }}
+                            className="text-2xl font-bold mb-2"
+                        >
+                            About Me
+                        </motion.h1>
+                        <p className="mb-4">
+                            Hi, I'm Ethan! I'm a Computer Science student at the University of California, Irvine. 
+                            My journey for programming started in 2015, where my curiosity for games met my love 
+                            for creativity and creation.
+                        </p>
+                        <p className="mb-4">
+                            I've been able to use my skills to create a twist on a classic game, Rock Paper Scissors, 
+                            to creating a website to help track my medications. Through each project, I've been able to build upon my technical skills while pushing 
+                            my innovativeness and creativity. Let's connect and create fresh, exciting applications 
+                            for the world!
+                        </p>
+                    </motion.div>                        
+                </div>
+                <div className="mx-auto container px-4 pb-8 mb-10">
                     <motion.h1 
                         initial={{ opacity: 0, x: -40 }}
                         whileInView={{ opacity: 1, x: 0 }}
@@ -111,33 +109,31 @@ const AboutSection = () => {
                         Education
                     </motion.h1>
 
-                    <div className="flex items-start mb-4">
+                    <div className="flex flex-col sm:flex-row items-start mb-4">
                         <img src={uci} alt="UCI Icon" className="w-12 h-12 mr-4 mt-2" />
                         <div className="flex-1">
-                            <div className="flex justify-between items-baseline">
+                            <div className="flex flex-col sm:flex-row sm:justify-between">
                                 <h2 className="text-lg font-semibold">University of California, Irvine</h2>
                                 <p className="text-base">Sep. 2023 – Present</p>
                             </div>
-                            <div className="flex justify-between items-baseline">
+                            <div className="flex flex-col sm:flex-row sm:justify-between">
                                 <p className="text-base text-gray-600">Bachelor of Science in Computer Science</p>
                                 <p className="text-sm">Irvine, CA</p>
                             </div>
-                            <p className="text-base text-gray-600">GPA: 3.86 / 4.0</p>
                         </div>
                     </div>
 
-                    <div className="flex items-start mb-8">
+                    <div className="flex flex-col sm:flex-row items-start mb-8">
                         <img src={mtsac} alt="MtSac Icon" className="w-12 h-12 mr-4 mt-2" />
                         <div className="flex-1">
-                            <div className="flex justify-between items-baseline">
+                            <div className="flex flex-col sm:flex-row sm:justify-between">
                                 <h2 className="text-lg font-semibold">Mount San Antonio College</h2>
                                 <p className="text-base">Aug. 2021 – June 2023</p>
                             </div>
-                            <div className="flex justify-between items-baseline">
+                            <div className="flex flex-col sm:flex-row sm:justify-between">
                                 <p className="text-base text-gray-600">Computer Science (Transfer)</p>
                                 <p className="text-sm">Walnut, CA</p>
                             </div>
-                            <p className="text-base text-gray-600">GPA: 3.85 / 4.0</p>
                         </div>
                     </div>
                     <Technologies/>
