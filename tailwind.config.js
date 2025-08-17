f2a298439f6bba56bd7acf8c4aa710ca738c926f
Ethan Lin <midgetlin2003@gmail.com>
2025-08-16 18:58:00 -0700

    Moved company logo inside icon and changed overall font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans'], // makes Roboto the default "sans" font
+      },
+    },
   },
   plugins: [],
 }
